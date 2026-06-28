7bf7ad73295e2374170f9872e2cbbdbfbf40b739
3zden <azeddineabouaam1@gmail.com>
2026-06-29 00:56:31 +0100

    build: add oauth2

--- user-service/src/main/java/org/aezden/Configurations/SecurityConfig.java
+++ user-service/src/main/java/org/aezden/Configurations/SecurityConfig.java
@@ -13,8 +13,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
         return http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(rs -> rs
-                        .requestMatchers("/users/**").permitAll()
+                        .requestMatchers("/users/register","/users/login").permitAll()
                         .anyRequest().authenticated())
                 .oauth2Login(Customizer.withDefaults())
                 .build();
